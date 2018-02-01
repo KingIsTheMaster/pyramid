55763a41b299adff3ae7cbe44ac683789a35ed3d
Joshua King <josh.king097@gmail.com>
2018-02-01 05:17:22 -0600

    Final Project

---
+++ PryramidSlider.js
@@ -0,0 +1,76 @@
+/**
+ * determineParametersAndThenDrawPyramid
+ *
+ * Determines the current value that the user has typed in the 'How high?' text-box,
+ * and then draws a pyramid with that height.
+ */
+function determineParametersAndThenDrawPyramid() {
+    
+    console.log("Someone called determineParametersAndThenDrawPyramid");
+
+    // figure out the height the user typed (replace the "5" below)
+    var heightStr = document.getElementById("rowSlider").value;
+    document.getElementById("heightInput").innerHTML = heightStr;
+
+    var character = document.getElementById("drawBrickSymbol").value;
+
+    // here we convert the string to an int
+    height = parseInt(heightStr);
+
+    // draw the pyramid with the given height
+    drawPyramid(height, character);
+
+    }
+    
+    var drawSymbol = document.getElementById("drawBrickSymbol");
+    drawSymbol.addEventListener("change", 
+    determineParametersAndThenDrawPyramid
+    );
+    
+    var rowSlider = document.getElementById("rowSlider");
+    rowSlider.addEventListener("input", 
+    determineParametersAndThenDrawPyramid
+    );
+    
+  
+    determineParametersAndThenDrawPyramid();
+    
+
+/**
+ * drawPyramid
+ *
+ * Renders, in the HTML document, a Mario pyramid of the specified height
+ */
+ function drawPyramid(height) {
+
+     // TODO 4
+     // before drawing, clear the old content
+     document.getElementById("pyramid").innerHTML = ""
+
+     // for each row....   
+    for (var row = 0; row < height; row++) {
+
+         // figure out number of bricks and spaces
+         var numBricks = row + 2;
+         var numSpaces = height - row - 1;
+
+         // build up a string for this row 
+         var rowStr = "";
+         for (var i = 0; i < numSpaces; i++) {
+             rowStr += " ";
+         }
+         for (var i = 0; i < numBricks; i++) {
+             rowStr += "#";
+         }
+
+        // create a text element with the string of characters
+        textElem = document.createTextNode(rowStr);
+
+        // create a <p> element with the text inside
+        rowElem = document.createElement("p");
+        rowElem.appendChild(textElem);
+
+        // insert the paragraph as a child of the container <div>
+        document.getElementById("pyramid").appendChild(rowElem);
+    }
+}
